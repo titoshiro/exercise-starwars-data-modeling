ab88225eec72c90977b857c3199b5b0657470a86
Héctor Silva Villena <120200972+titoshiro@users.noreply.github.com>
2023-06-17 22:18:50 +0000

    entrega

--- src/models.py
+++ src/models.py
@@ -5,25 +5,103 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# Tabla de usuarios
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuarios'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    email = Column(String(250), unique=True, nullable=False)
+    password = Column(String(250), nullable=False)
+    nombre = Column(String(250))
+    apellido = Column(String(250))
+    fecha_de_subscripción = Column(String(250))
+
+    favoritos_planetas = relationship('Planeta', secondary='favoritos')
+    favoritos_personajes = relationship('Personaje', secondary='favoritos')
+
+
+class Planeta(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(255), unique=True, nullable=False)
+    período_de_rotación = Column(String(250))
+    período_orbital = Column(String(250))
+    diámetro = Column(String(250))
+    clima = Column(String(250)) 
+    gravedad = Column(String(250))
+    terreno = Column(String(250))
+    agua_superficial = Column(String(250))
+    población = Column(String(250))
+    residentes = Column(String(250))
+
+    # Relación Uno-a-muchos con Comentario
+    comentarios = relationship('Comentario')
+
+    # Relación Uno-a-muchos con Favorito
+    favoritos = relationship('Favorito')
+
+
+class Personaje(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), unique=True, nullable=False)
+    altura = Column(String(250))
+    peso = Column(String(250))
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    color_de_ojos = Column(String(250))
+    birth_year = Column(String(250))
+    género = Column(String(250))
+    mundo_natal = Column(String(250))
+    vehículos = Column(String(250))
+    naves_estelares = Column(String(250))
+
+    # Relación Uno-a-muchos con Comentario
+    comentarios = relationship('Comentario')
+
+    # Relación Uno-a-muchos con Favorito
+    favoritos = relationship('Favorito')
+
+
+class Comentario(Base):
+    __tablename__ = 'comentarios'
+    id = Column(Integer, primary_key=True)
+    contenido = Column(String(250), nullable=False)
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
+    planeta_id = Column(Integer, ForeignKey('planetas.id'))
+    personaje_id = Column(Integer, ForeignKey('personajes.id'))
+
+    # Relación Muchos-a-uno con Usuario
+    usuario = relationship('Usuario')
+
+    # Relación Muchos-a-uno con Planeta
+    planeta = relationship('Planeta')
+
+    # Relación Muchos-a-uno con Personaje
+    personaje = relationship('Personaje')
+
+
+class Favorito(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
+    planeta_id = Column(Integer, ForeignKey('planetas.id'))
+    personaje_id = Column(Integer, ForeignKey('personajes.id'))
+
+    # Relación Muchos-a-uno con Usuario
+    usuario = relationship('Usuario')
+
+    # Relación Muchos-a-uno con Planeta
+    planeta = relationship('Planeta')
+
+    # Relación Muchos-a-uno con Personaje
+    personaje = relationship('Personaje')
+
+
+
 
     def to_dict(self):
         return {}
